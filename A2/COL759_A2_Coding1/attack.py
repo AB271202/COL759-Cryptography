d25c4ddc0b262b3e52dfe13bd53f2fc519511774
AB271202 <anishbanerjee2002@gmail.com>
2023-09-27 00:45:16 +0530

    Coding 1 done

--- A2/COL759_A2_Coding1/attack.py
+++ A2/COL759_A2_Coding1/attack.py
@@ -1,7 +1,7 @@
 """
 You can implement helper function here if you want
 """
-
+# import perm
 def attack(oracle, pi_func, p):
     """
     The challenger has sampled a bit b <- {0, 1} uniformly randomly:
@@ -23,9 +23,21 @@ def attack(oracle, pi_func, p):
 
     TODO: Implement your code below
     """
+    y0=oracle(0)
+    y1=oracle(1)
+    y2=oracle(2)
+    k1=((2*((y1-y2)%p))*pi_func((y0+y2-2*y1)%p))%p
+    k2=(y1-k1*(y0-y1))%p
+    y0new=(pi_func(k1)+k2)%p
+    y1new=(pi_func((k1+1)%p)+k2)%p
+    y2new=(pi_func((k1+2)%p)+k2)%p
+    if y0==y0new and y1==y1new and y2==y2new:
+        return 0
+    else:
+        return 1
+    
 
-
-    """
-    Return guess of b (either 0 or 1)
-    """
-    return None
+if __name__=="__main__":
+    p=perm.prp_oracle()
+    # print(p.k1,p.k2)
+    print(attack(p.oracle,p.pi_func,p.p))
