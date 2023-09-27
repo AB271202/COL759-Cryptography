571aad87bae8dc4066a2c26193ba88371c752088
AB271202 <anishbanerjee2002@gmail.com>
2023-09-27 21:06:02 +0530

    Coding 4 for 16 one block

--- A2/COL759_A2_Coding4/attack.py
+++ A2/COL759_A2_Coding4/attack.py
@@ -15,6 +15,8 @@ def attack(cipher_text):
     print(cipher_text)
     iv = cipher_text[:16]
     first = cipher_text[16:32]
+    
+    # Find padding
     padding = 0
     for i in range(15, -1, -1):
         iv[i] = (iv[i] + 1) % NUM
@@ -24,21 +26,19 @@ def attack(cipher_text):
             break
         iv[i] = (iv[i] - 1) % NUM
     print("Padding: ",padding)
-    print(iv)
-    print(first)
+
     message = []
-    for i in range(padding, padding+1):
+    for i in range(padding, 16):
         for j in range(i):
-            iv[j] = (iv[j]^(padding+i)^(padding+i+1))
-        # print(iv)
-        # check_padding(iv+first)
+            iv[j] = (iv[j]^(i)^(i+1))
+
         for k in range(256):
-            iv[i]=(iv[i]^((padding+i+1)^k))
+            iv[i]=iv[i]^((i+1)^k)
             if check_padding(iv+first)==0:
-                print(i)
-                message.append(i)
+                message.append(k)
                 break
-            iv[i]=(iv[i]^((padding+i+1)^k))
+            iv[i]=iv[i]^((i+1)^k)
+            
     """
     Return a list of integers representing the original message
     """
@@ -47,6 +47,9 @@ def attack(cipher_text):
 
 if __name__ == "__main__":
     string = "Hello World"
-    print(list((bytes(string, 'raw_unicode_escape'))))
-    print(len(list((bytes(string, 'raw_unicode_escape')))))
-    print(attack(encrypt(list(bytes(string, 'raw_unicode_escape')))))
+    initial = (list((bytes(string, 'raw_unicode_escape'))))
+    print(initial)
+    print(len(initial))
+    final=attack(encrypt(initial))
+    print(final)
+    print(initial==final)
