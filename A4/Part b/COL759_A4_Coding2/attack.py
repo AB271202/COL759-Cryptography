07128bd102b8184f59263910a29fa706fccb12f0
AB271202 <anishbanerjee2002@gmail.com>
2023-11-25 23:07:03 +0530

    Assignment 3 stuff

---
+++ A4/Part b/COL759_A4_Coding2/attack.py
@@ -0,0 +1,14 @@
+"""
+You can define helper function here if you want
+"""
+
+def attack(N, e, msg):
+    """
+    write the code below
+    """
+    
+
+    """
+    return a signature (integer) corresponding to the given message that will get get verified
+    """
+    return 0
---
+++ A4/Part b/COL759_A4_Coding3/attack.py
@@ -0,0 +1,19 @@
+from rsa import check_padding
+
+"""
+Takes a ciphertext, public modulus and public exponent as input as input
+PARAMS:
+ciphertext: a list of integers of size 128 bytes
+N: the public modulus of size 128 bytes
+e: the public exponent
+"""
+def attack(cipher_text, N, e):
+    """
+    TODO: Implement your code here
+    """
+    
+
+    """
+    Return a list of integers representing the original message
+    """
+    return []
