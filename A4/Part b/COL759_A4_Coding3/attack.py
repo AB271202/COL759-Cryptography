fd468a4d90eae3c905d946872c660134a4f559c4
AB271202 <anishbanerjee2002@gmail.com>
2023-11-28 13:12:44 +0530

    Current status

--- A4/Part b/COL759_A4_Coding3/attack.py
+++ A4/Part b/COL759_A4_Coding3/attack.py
@@ -1,4 +1,5 @@
 from rsa import check_padding, encryption, pad
+# from rsa import check_padding
 import math
 """
 Takes a ciphertext, public modulus and public exponent as input as input
@@ -9,50 +10,6 @@ e: the public exponent
 """
 
 pub_key = (3, 783948438612447530520500845779523629098681116915315373171090024704252529272105569277652766190092846991058906985549067848089611892705206703102883806613132978957364381750756122225194752372805842732963804787636530587121476113137183873)
-
-
-def step2(M, s, B, N, ct, e):
-    # Find s
-    if len(M) == 1:
-        for (a,b) in M:
-            r = 2*(b*s - 2*B)//N
-            while True:
-                s1 = (2*B+r*N)//b
-                found = 0
-                while s1 < (3*B+r*N)//a:
-                    if check_padding(to_list((ct*pow(s1, e, N)) % N)):
-                        found = 1
-                        break
-                    s1 += 1
-                if found:
-                    break
-                else:
-                    r += 1
-    else:
-        print("M has multiple intervals", M.size())
-        s1=s+1
-        while s1 < (3*B):
-            if check_padding(to_list((ct*pow(s1, e, N)) % N)):
-                break
-            s1 += 1
-    return s1
-
-
-def step3(s1, M, B, N):
-    # Add to the set
-    M1 = set()
-    for element in M:
-        a,b = element
-        r = (a*s1-3*B+1)//N
-        # print(a,b)
-        while r<=(b*s1-2*B)//N:
-            # print((3*B+r*N)//s1)
-            alpha = max(a, math.ceil((2*B+r*N)/s1))
-            beta = min(b, math.floor((3*B+r*N)/s1))
-            if alpha <= beta:
-                M1.add((alpha, beta))
-            r += 1
-    return M1
 
 
 def to_int(cipher_text):
@@ -69,14 +26,60 @@ def to_list(ct):
     return cipher_text[::-1]
 
 
+def step2(M, s, B, N, ct, e):
+    # Find s
+    if len(M) == 1:
+        for (a,b) in M:
+            r = math.ceil(2*((b*s - 2*B)/N))
+            while True:
+                s1 = (2*B+r*N)//b
+                found = 0
+                while s1 < (3*B+r*N)/a:
+                    if check_padding(to_list((ct*pow(s1, e, N)) % N)):
+                        found = 1
+                        break
+                    s1 += 1
+                if found:
+                    break
+                else:
+                    r += 1
+    else:
+        print("M has multiple intervals ", len(M))
+        s1=s+1
+        while s1 < (3*B):
+            if check_padding(to_list((ct*pow(s1, e, N)) % N)):
+                break
+            s1 += 1
+    return s1
+
+
+def step3(s, M, B, N):
+    # Add to the set
+    M1 = set()
+    for element in M:
+        a,b = element
+        r = (a*s-3*B+1)//N
+        # print(a,b)
+        while r<=(b*s-2*B)//N:
+            # print((3*B+r*N)//s)
+            alpha = max(a, math.ceil((2*B+r*N)/s))
+            beta = min(b, math.floor((3*B-1+r*N)/s))
+            if alpha <= beta:
+                M1.add((alpha, beta))
+            r += 1
+    return M1
+
 def attack(cipher_text, N, e):
     """
     TODO: Implement your code here
     """
+    
     k = N.bit_length()//8
     B = 2**(8*(k-2))
-    s = N//(3*B)
     ct = to_int(cipher_text)
+
+    # First s
+    s = N//(3*B)
     while s < 3*B:
         if check_padding(to_list((ct*pow(s, e, N)) % N)):
             break
@@ -91,7 +94,7 @@ def attack(cipher_text, N, e):
     M = set()
     while (3*B+r*N)//s < b:
         alpha = max(a, math.ceil((2*B+r*N)/s))
-        beta = min(b, math.floor((3*B+r*N)/s))
+        beta = min(b, math.floor((3*B+r*N-1)/s))
         if alpha <= beta:
             M.add((alpha, beta))
         r += 1
@@ -102,13 +105,12 @@ def attack(cipher_text, N, e):
         M = step3(s, M, B, N)
         print(M)
         if len(M) == 1:
-            x = M.pop()
-            if x[0] == x[1]:
-                pm=to_list(M)
-                index=M.find(0)
-                return M[index+1:]
-            else:
-                M.add(x)
+            for x in M:
+                if x[0] == x[1]:
+                    pm=to_list(x[0])
+                    index=pm.index(0)
+                    return pm[index+1:]
+            
     """
     Return a list of integers representing the original message
     """
